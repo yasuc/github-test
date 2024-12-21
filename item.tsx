a3e14aec457a4c48fb570bd322c20fa4a4ca3d80
yasuc18 <yasuc18@gmail.com>
2024-12-21 22:46:53 +0900

    feat: 🎸 商品管理ページの作成 (#14)
    issue #13に基づいて商品管理ページを実装
    
    ✅ Closes: #13

---
+++ item.tsx
@@ -0,0 +1,13 @@
+// ▼商品一覧画面の表示
+export default function ItemList() {
+  return (
+    <main>
+      <h1>商品一覧画面</h1>
+      <ul>
+        <li>商品ID</li>
+        <li>商品名</li>
+        <li>価格</li>
+      </ul>
+    </main>
+  );
+}
