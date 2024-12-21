953197857549514f77496d2771d58cc1b4107741
yasuc18 <yasuc18@gmail.com>
2024-12-21 18:13:29 +0900

    fix: 🐛 ユーザ検索画面の修正 (#12)
    ユーザID等を表示するように変更
    
    ✅ Closes: #9

--- user.tsx
+++ user.tsx
@@ -3,6 +3,11 @@ export default function UserSearch() {
   return (
     <main>
       <h1>ユーザ検索画面</h1>
+      <ul>
+        <li>ユーザID</li>
+        <li>ユーザ名</li>
+        <li>管理権限</li>
+      </ul>
     </main>
   );
 }
