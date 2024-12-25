54f49ba62c43357aaede7e27da54360cfda06ddf
IHA Yasushi <yasuc18@gmail.com>
2024-12-25 21:44:52 +0900

    管理画面の変更

--- root.tsx
+++ root.tsx
@@ -1,3 +1,13 @@
-export default (first) => {
-  return <div>{first}</div>;
+//管理画面を表示するコンポーネント
+
+export default function Admin() {
+  return (
+    <div>
+      <h1>管理画面</h1>
+      <ul>
+        <li>ユーザ管理</li>
+        <li>掲示板管理</li>
+      </ul>
+    </div>
+  );
 }
